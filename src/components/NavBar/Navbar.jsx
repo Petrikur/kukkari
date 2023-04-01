020bb64da629a9a259a2608f6ed66f6c28048b02
petrikur <patekuronen123@gmail.com>
2023-04-01 18:17:28 +0300

    navbar + tailwind

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function HumbleiconsBars(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      ></path>
+    </svg>
+  );
+}
+
+const Navbar = ({ toggle }) => {
+  return (
+    <nav className="shadow-lg fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-black to-cool-gray-800">
+      <div className="container mx-auto px-6 md:px-0 py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <button
+            type="button"
+            className="block md:hidden border border-gray-800 p-2 rounded-md text-white focus:outline-none focus:shadow-outline"
+            onClick={toggle}
+          >
+            <HumbleiconsBars />
+          </button>
+        </div>
+        <div className="hidden md:block">
+          <ul className="inline-flex">
+            <li>
+              <Link to="/">Etusivu</Link>
+            </li>
+            <>
+              <li>
+                <Link to="/galleria">Galleria</Link>
+              </li>
+              <li>
+                <Link to="/rakennukset">Rakennukset</Link>
+              </li>
+              <li>
+                <Link to="/varausvuorot">Varausvuorot</Link>
+              </li>
+              <li>
+                <Link to="/yllapito">Ylläpito</Link>
+              </li>
+            </>
+          </ul>
+        </div>
+        <div className="hidden md:block">
+          <Link
+            to="/auth"
+            className="inline-block bg-gray-800 text-white py-2 px-4 rounded-md transition-colors duration-300 hover:bg-gray-900"
+          >
+            Kirjaudu
+          </Link>
+
+          {/* <button
+            type="button"
+            className="inline-block bg-gray-800 text-white py-2 px-4 rounded-md transition-colors duration-300 hover:bg-gray-900"
+          >
+            Kirjaudu Ulos
+          </button> */}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
