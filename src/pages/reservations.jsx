15f447c562575b6e1aeb37019eeab24811817da5
petrikur <patekuronen123@gmail.com>
2023-04-15 23:10:16 +0300

    add reservation calendar

--- src/pages/reservations.jsx
+++ src/pages/reservations.jsx
@@ -1,47 +1,173 @@
-import { DateRangePicker } from "react-date-range";
-import "react-date-range/dist/styles.css"; 
-import "react-date-range/dist/theme/default.css"; 
-import { addDays } from "date-fns";
-import { useEffect, useState } from "react";
-import {fi} from 'react-date-range/dist/locale';
-import {format} from "date-fns"
 import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import axios from "axios";
+import { fi } from "react-date-range/dist/locale";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../Ui/LoadingSpinner";
+import { AuthContext } from "../components/context/authContext";
+import eachDayOfInterval from "date-fns/eachDayOfInterval/index";
 
-const Reservations = (props) => {
+const Reservations = () => {
+  const auth = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState();
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const navigate = useNavigate();
+  const [disabledDates, setDisabledDates] = useState([]);
 
-  const [date, setDate] = useState([
+  const [loadedReservations, setLoadedReservations] = useState([]);
+
+  const [selectedDate, setSelectedDate] = useState([
     {
       startDate: new Date(),
-      endDate: addDays(new Date(), 7),
+      endDate: new Date(),
       key: "selection",
     },
   ]);
 
+  const [events, setEvents] = useState([]);
+
+  const getReservations = async () => {
+    setIsLoading(true);
+    try {
+      const responseData = await axios(
+        "http://localhost:5000/api/reservations",
+        {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        }
+      );
+
+      setLoadedReservations(responseData.data.reservations);
+      setIsLoading(false);
+
+      navigate("/reservations");
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    console.log(`${format(date[0].startDate, "dd/MM/yy")} ${format(date[0].endDate, "dd/MM/yy")}`)
-  },[date])
+    getReservations();
+  }, []);
 
-  const dateHandler = () => {
+  useEffect(() => {
+    handleDisabledDates();
 
-  }
+    convertDisabledDatesToEvents();
+  }, [loadedReservations]);
+
+  const handleDisabledDates = () => {
+    let disabled = [];
+    for (let dateObj of loadedReservations) {
+      const startDate = new Date(dateObj.startDate);
+      const endDate = new Date(dateObj.endDate);
+      const datesToAdd = eachDayOfInterval({ start: startDate, end: endDate });
+      disabled = [...disabled, ...datesToAdd];
+    }
+    setDisabledDates(disabled);
+  };
+
+  const handleButtonClick = () => {
+    setShowDatePicker(true);
+  };
+
+  const submitReservation = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/reservations/",
+        {
+          startDate: selectedDate[0].startDate,
+          endDate: selectedDate[0].endDate,
+          creatorName: auth.name,
+          userId: auth.userId,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + auth.token,
+          },
+        }
+      );
+      toast.success("Varaus tehty");
+      setIsLoading(false);
+      setLoadedReservations([...loadedReservations, response.data.reservation]);
+      navigate("/reservations");
+      setShowDatePicker(false);
+    } catch (err) {
+      console.log(err);
+      setShowDatePicker(false);
+      const errorMessage = err.response.data.message;
+      toast.warn(errorMessage);
+      setIsLoading(false);
+      console.log(errorMessage);
+    }
+  };
+
+  const convertDisabledDatesToEvents = () => {
+    const events = [];
+    loadedReservations.forEach((date) => {
+      console.log("daadada: ", date);
+      events.push({
+        title: auth.name,
+        start: date.startDate,
+        end: date.endDate,
+        backgroundColor: "red",
+        borderColor: "red",
+      });
+    });
+    setEvents(events);
+  };
+
   return (
-    <>
-      {" "}
-      <div className="py-20 flex items-center justify-center">
-        <h1 className="text-2xl font-bold">Varaukset </h1>
-        <div>
-          <DateRangePicker
-            onChange={(item) => setDate([item.selection])}
-            // showSelectionPreview={true}
-            moveRangeOnFirstSelection={false}
-            months={1}
-            ranges={date}
-            direction="horizontal"
-            locale={fi}
-          />
+    <div className="flex items-center justify-center pt-28 flex-col py-2 px-4 ">
+    
+      {isLoading && <LoadingSpinner asOverlay />}
+      <div className="text-white">
+        <h1>Varaukset</h1>
+        <div className="">
+          Voit tehdä varauksia painamalla tee varaus täällä. Alla olevassa
+          kalenterissa näät jo varatut ajat. Jos perut varamaamasi ajan, muista
+          poistaa varauksesi{" "}
         </div>
       </div>
-    </>
+      <button
+        className="my-4 px-4 py-2 text-lg font-medium text-white bg-green-500 rounded-md hover:bg-green-600"
+        onClick={handleButtonClick}
+      >
+        Tee varaus
+      </button>
+      {showDatePicker && (
+        <React.Fragment>
+          {" "}
+          <DateRange
+            disabledDates={disabledDates}
+            locale={fi}
+            editableDateInputs={true}
+            onChange={(item) => setSelectedDate([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={selectedDate}
+          />
+          <div className="flex items-end justify-center">
+            <button
+              className="my-4 px-4 py-2 text-lg font-medium text-white bg-green-500 rounded-md hover:bg-green-600"
+              onClick={submitReservation}
+            >
+              Varaa
+            </button>
+          </div>
+        </React.Fragment>
+      )}
+    
+      <ToastContainer />
+    </div>
   );
 };
 
