cbdb352a46609ad29c61e5628c666bb01636745c
petrikur <patekuronen123@gmail.com>
2023-04-16 23:55:36 +0300

    add large calendar

--- src/pages/reservations.jsx
+++ src/pages/reservations.jsx
@@ -1,35 +1,56 @@
 import React from "react";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useCallback } from "react";
 import { DateRange } from "react-date-range";
-import "react-date-range/dist/styles.css"; // main css file
-import "react-date-range/dist/theme/default.css"; // theme css file
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 import axios from "axios";
-import { fi } from "react-date-range/dist/locale";
 import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../Ui/LoadingSpinner";
 import { AuthContext } from "../components/context/authContext";
 import eachDayOfInterval from "date-fns/eachDayOfInterval/index";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import fi from "date-fns/locale/fi";
+import Modal from "../Ui/Modal";
+import { addDays } from 'date-fns';
+import "./calendar.css";
 
 const Reservations = () => {
+  const locales = {
+    fi,
+  };
+
+  // Create localizer for big calendar
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek: (date) => startOfWeek(date, { weekStartsOn: 1 }),
+    getDay,
+    locales: { fi: fi },
+  });
   const auth = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState();
   const [showDatePicker, setShowDatePicker] = useState(false);
   const navigate = useNavigate();
   const [disabledDates, setDisabledDates] = useState([]);
-
   const [loadedReservations, setLoadedReservations] = useState([]);
-
+  const [events, setEvents] = useState([]);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState({});
   const [selectedDate, setSelectedDate] = useState([
     {
       startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
+      endDate: addDays(new Date(), 3),
+      key: 'selection'
     },
   ]);
 
-  const [events, setEvents] = useState([]);
-
+  // Get reservations
   const getReservations = async () => {
     setIsLoading(true);
     try {
@@ -44,24 +65,23 @@ const Reservations = () => {
 
       setLoadedReservations(responseData.data.reservations);
       setIsLoading(false);
-
       navigate("/reservations");
     } catch (err) {
       console.log(err);
       setIsLoading(false);
     }
   };
-
+  // Get reservations
   useEffect(() => {
     getReservations();
   }, []);
 
   useEffect(() => {
     handleDisabledDates();
-
     convertDisabledDatesToEvents();
   }, [loadedReservations]);
 
+  // Convert loadedReservations to disabled dates in calendar component
   const handleDisabledDates = () => {
     let disabled = [];
     for (let dateObj of loadedReservations) {
@@ -73,13 +93,36 @@ const Reservations = () => {
     setDisabledDates(disabled);
   };
 
-  const handleButtonClick = () => {
+  // Open reservation calendar when button click
+  const openCalendar = () => {
     setShowDatePicker(true);
+    setSelectedDate([{start: new Date(), end:new Date(),key: "selection"}])
   };
 
+  // Show disabled dates as events in larger calendar
+  const convertDisabledDatesToEvents = () => {
+    const events = [];
+    loadedReservations.forEach((date) => {
+      const startDate = new Date(date.startDate);
+      const endDate = new Date(date.endDate);
+      endDate.setDate(endDate.getDate() + 1);
+      events.push({
+        _id: date._id,
+        title: "Varattu / " + auth.name,
+        start: startDate,
+        end: endDate,
+        allDay: true,
+      });
+    });
+    setEvents(events);
+  };
+
+  // Make new reservation
   const submitReservation = async (event) => {
     event.preventDefault();
     setIsLoading(true);
+
+    
     try {
       const response = await axios.post(
         "http://localhost:5000/api/reservations/",
@@ -96,66 +139,158 @@ const Reservations = () => {
           },
         }
       );
-      toast.success("Varaus tehty");
-      setIsLoading(false);
-      setLoadedReservations([...loadedReservations, response.data.reservation]);
-      navigate("/reservations");
-      setShowDatePicker(false);
+      setTimeout(() => {
+        setLoadedReservations([
+          ...loadedReservations,
+          response.data.reservation,
+        ]);
+        toast.success("Varaus tehty");
+        setIsLoading(false);
+      }, 500);
     } catch (err) {
-      console.log(err);
-      setShowDatePicker(false);
       const errorMessage = err.response.data.message;
       toast.warn(errorMessage);
-      setIsLoading(false);
       console.log(errorMessage);
+    } finally {
+      setShowDatePicker(false);
+      navigate("/reservations");
     }
   };
 
-  const convertDisabledDatesToEvents = () => {
-    const events = [];
-    loadedReservations.forEach((date) => {
-      console.log("daadada: ", date);
-      events.push({
-        title: auth.name,
-        start: date.startDate,
-        end: date.endDate,
-        backgroundColor: "red",
-        borderColor: "red",
+  
+
+  // Delete events
+  const handleDeleteEvent = async (_id) => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`http://localhost:5000/api/reservations/${_id}`, {
+        headers: {
+          Authorization: `Bearer ${auth.token}`,
+        },
       });
-    });
-    setEvents(events);
+      setEvents(events.filter((e) => e._id !== _id));
+      setLoadedReservations(loadedReservations.filter((e) => e._id !== _id));
+      toast.success("Event deleted!");
+      navigate("/reservations");
+    } catch (err) {
+      const errorMessage = err.response.data.message;
+      toast.error(errorMessage);
+    } finally {
+      setShowConfirmModal(false);
+      setIsLoading(false);
+    }
   };
 
+  const handleEventClick = (event) => {
+    setShowConfirmModal(true);
+    setSelectedEvent(event);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  // style event
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const backgroundColor = isSelected ? "blue" : "red";
+    const hoverStyle = {
+      backgroundColor: "orange",
+      cursor: "pointer",
+    };
+    const style = {
+      backgroundColor,
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+      border: "0px",
+      display: "block",
+      fontSize: "14px",
+      padding: "8px",
+    };
+
+    return {
+      style: isSelected ? { ...style, ...hoverStyle } : style,
+    };
+  };
+  // const handleSelect = (ranges) => {
+  //   setDateRange(ranges);
+  // };
+  // const handleSelect = (ranges) => {
+  //   setDateRange(ranges.selection);
+  // };
   return (
     <div className="flex items-center justify-center pt-28 flex-col py-2 px-4 ">
-    
+      <Modal
+        show={showConfirmModal}
+        onClose={cancelDeleteHandler}
+        header="Oletko varma?"
+        footer={
+          <React.Fragment>
+            <div className="flex ">
+              <button
+                className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                onClick={cancelDeleteHandler}
+              >
+                Peruuta
+              </button>
+              <button
+                className="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-600 ml-2"
+                onClick={() => {
+                  handleDeleteEvent(selectedEvent._id);
+                }}
+              >
+                Poista
+              </button>
+            </div>
+          </React.Fragment>
+        }
+      >
+        <p>Oletko varma että haluat poistaa varauksen?</p>
+      </Modal>
       {isLoading && <LoadingSpinner asOverlay />}
-      <div className="text-white">
-        <h1>Varaukset</h1>
-        <div className="">
+      <div className="text-white mb-20 lg:px-52">
+        <h1 className="text-4xl mb-5 ">Varaukset</h1>
+        <div className="text-md">
           Voit tehdä varauksia painamalla tee varaus täällä. Alla olevassa
           kalenterissa näät jo varatut ajat. Jos perut varamaamasi ajan, muista
-          poistaa varauksesi{" "}
+          poistaa varauksesi. Voit poistaa oman varauksesi klikkaamalla isosta
+          kalenterista varausta ja poistaa sen etkön tahju.
+        </div>
+        <div className="my-2">
+          Voit varata kerralla ajan joko yhdelle päivälle tai useammalle
+          päivälle. Useammalle päivälle varatessa klikkaa alkupäivämäärää ja sen
+          jälkeen loppupäivämäärää ja sitten varaa.
         </div>
       </div>
-      <button
-        className="my-4 px-4 py-2 text-lg font-medium text-white bg-green-500 rounded-md hover:bg-green-600"
-        onClick={handleButtonClick}
-      >
-        Tee varaus
-      </button>
+      {!showDatePicker && (
+        <button
+          className="my-4 px-4 py-2 text-lg font-medium text-white bg-green-500 rounded-md hover:bg-green-600"
+          onClick={openCalendar}
+        >
+          Tee varaus
+        </button>
+      )}
       {showDatePicker && (
         <React.Fragment>
           {" "}
           <DateRange
             disabledDates={disabledDates}
             locale={fi}
-            editableDateInputs={true}
             onChange={(item) => setSelectedDate([item.selection])}
             moveRangeOnFirstSelection={false}
             ranges={selectedDate}
+           
+     
           />
-          <div className="flex items-end justify-center">
+          <div className="flex items-end justify-center gap-2">
+            <button
+              className="my-4 px-4 py-2 text-lg font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600"
+              onClick={() => {
+                setShowDatePicker(false);
+              }}
+            >
+              Peruuta
+            </button>
             <button
               className="my-4 px-4 py-2 text-lg font-medium text-white bg-green-500 rounded-md hover:bg-green-600"
               onClick={submitReservation}
@@ -165,7 +300,20 @@ const Reservations = () => {
           </div>
         </React.Fragment>
       )}
-    
+      <div className="mx-auto w-full lg:w-3/4 xl:w-3/4 mb-20">
+        <Calendar
+          events={events}
+          localizer={localizer}
+          style={{ height: 600 }}
+          onSelectEvent={handleEventClick}
+          eventPropGetter={eventStyleGetter}
+          toolbar={{
+            left: "",
+            center: "",
+            right: "prev,next",
+          }}
+        />
+      </div>
       <ToastContainer />
     </div>
   );
