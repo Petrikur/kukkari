00985a844a41aadd65aa72f8fd2d83a654009fb5
petrikur <patekuronen123@gmail.com>
2023-04-19 17:09:15 +0300

    user feedback pw reset

--- src/pages/forgotPasswordPage.jsx
+++ src/pages/forgotPasswordPage.jsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
 import axios from "axios";
-import {toast,ToastContainer} from "react-toastify"
+import { toast, ToastContainer } from "react-toastify";
 
 const ForgotPasswordPage = () => {
   const [email, setEmail] = useState("");
+  const [requestSend, setRequestSend] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/users/forgotpassword", { email });
-      toast.success(response.data)
+      const response = await axios.post(
+        "http://localhost:5000/api/users/forgotpassword",
+        { email }
+      );
+      toast.success(response.data);
     } catch (error) {
       console.error(error);
+      toast.warn(error.response.data.message)
+    } finally {
+      setRequestSend(true);
     }
   };
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <form
-        className="w-full max-w-md bg-white p-8 rounded-lg shadow-md"
-        onSubmit={handleSubmit}
-      >
-        {" "}
-        <h2 className="text-2xl font-medium mb-6">Salasanan vaihto</h2>
-        <label htmlFor="email">Email:</label>
-        <input
-          className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-5"
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
-        />
-        <button
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
+      {requestSend ? (
+        <div className="text-3xl p-5 border border-white rounded text-white">Pyyntö lähetetty, tarkista sähköpostisi!</div>
+      ) : (
+        <form
+          className="w-full max-w-md bg-white p-8 rounded-lg shadow-md"
+          onSubmit={handleSubmit}
         >
-          Vaihda salasana
-        </button>
-      </form>
+          {" "}
+          <h2 className="text-2xl font-medium mb-6">Salasanan vaihto</h2>
+          <label htmlFor="email">Email:</label>
+          <input
+            className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-5"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          <button
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Vaihda salasana
+          </button>
+        </form>
+      )}
+
       <ToastContainer />
     </div>
   );
--- src/pages/passwordResetPage.jsx
+++ src/pages/passwordResetPage.jsx
@@ -1,75 +1,97 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import LoadingSpinner from "../Ui/LoadingSpinner";
 
 const ResetPassword = () => {
-  const {id,token} = useParams();
+  const { id, token } = useParams();
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [validationError, setValidationError] = useState("");
+  const [requestSend,setRequestSend] = useState(false);
+  const [isLoading,setIsLoading] = useState(false)
+  const [passwordChanged, setPasswordChanged] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      setValidationError("Passwords do not match");
+      setValidationError("Salasanat eivät täsmää!");
       return;
     }
     try {
+      
       const response = await axios.post(
         `http://localhost:5000/api/users/${id}/${token}`,
         {
-          password
+          password,
         }
       );
-
+      if (response.status === 200) {
+        setPasswordChanged(true);
+      }
+      
     } catch (error) {
       console.error(error);
+      toast.warn(error.response.data.message)
+    }finally{
+      setRequestSend(true)
+      setIsLoading(false)
     }
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-md bg-white p-8 rounded-lg shadow-md"
-      >
-        <h2 className="text-2xl font-medium mb-6">Reset Password</h2>
-        <div className="mb-4">
-          <label htmlFor="password" className="block font-medium mb-2">
-            New Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
+    {isLoading && <LoadingSpinner/>}
+      {requestSend && passwordChanged  ? (
+        <div className="text-3xl p-5 border border-white rounded text-white">
+          Salasana vaihdettu, voit nyt kirjautua sisään.
         </div>
-        <div className="mb-6">
-          <label htmlFor="confirm-password" className="block font-medium mb-2">
-            Confirm Password:
-          </label>
-          <input
-            type="password"
-            id="confirm-password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
-          {validationError && (
-            <p className="text-red-500 text-sm mt-2">{validationError}</p>
-          )}
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white p-8 rounded-lg shadow-md"
         >
-          Reset Password
-        </button>
-      </form>
+          <h2 className="text-2xl font-medium mb-6">Vaihda salasana</h2>
+          <div className="mb-4">
+            <label htmlFor="password" className="block font-medium mb-2">
+              Uusi salasana:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="confirm-password"
+              className="block font-medium mb-2"
+            >
+              Vahvista salasana:
+            </label>
+            <input
+              type="password"
+              id="confirm-password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+            {validationError && (
+              <p className="text-red-500 text-sm mt-2">{validationError}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Reset Password
+          </button>
+        </form>
+      )}
     </div>
   );
 };
