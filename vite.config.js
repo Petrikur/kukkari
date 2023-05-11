37e78c17af48309b2df421ee785dbef830e235d7
petrikur <patekuronen123@gmail.com>
2023-05-11 03:29:54 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react() , pluginRewriteAll()],
   server: {
-    publicPath:`${import.meta.env.VITE_SERVER_URL}`
-  }
+    proxy: {
+      '/api': {
+        target: process.env.VITE_SERVER_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
-
